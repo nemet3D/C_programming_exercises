deee8d15e540c431481710843c063010bc5ad8b4
nemet3D <nemet3d@gmail.com>
2018-09-28 18:55:30 +0300

    Exercise_3
    Exercise number 3.

---
+++ 201809/3.c
@@ -0,0 +1,32 @@
+/*
+Write a C program to print a big 'C'.
+Expected Output:
+
+    ######
+  ##      ##
+ #
+ #
+ #
+ #
+ #
+  ##      ##
+    ######
+*/
+
+// Includes
+#include <stdio.h>
+
+// Variables
+
+int main(void)
+{
+	printf("   #####\n");
+	printf(" ##     ##\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf(" ##     ##\n");
+	printf("   #####\n");
+}
