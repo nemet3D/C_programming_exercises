f7850fa355ad1be993e0e514889fc0ab88a5a74f
nemet3D <nemet3d@gmail.com>
2018-09-28 18:50:31 +0300

    2_exercise
    Second eexercise of the list.

---
+++ 201809/2.c
@@ -0,0 +1,29 @@
+/*
+Write a C program to print a block F using hash (#), where the F has a height of six characters and width of five and four characters. Go to the editor
+Expected Output:
+
+######
+#
+#
+#####
+#
+#
+#
+*/
+
+// Includes
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(void)
+{
+	printf("######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+}
